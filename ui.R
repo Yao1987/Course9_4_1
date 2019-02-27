35fb5c1e60726302286f0d888ed2724b56a1ce07
Yao1987 <39341146+Yao1987@users.noreply.github.com>
2019-02-27 16:10:09 +0100

    9_4

--- ui.R
+++ ui.R
@@ -9,28 +9,31 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
-  # Application title
   titlePanel("Calories consumption Calculator"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       numericInput("walking_hours",label=h4("Hours of walking"),min = 0, max = 1000,0),
       numericInput("running_hours",label=h4("Hours of running"),min = 0, max = 1000,0),
       numericInput("swimming_hours", label=h4("Hours of swimming"),min = 0, max = 1000,0), 
-      actionButton("CalcCalories", label="Your Calories intake is")
+      actionButton("CalcCalories", label="Your Calories consumption is")
     ),
     
-    # Show a plot of the generated distribution
+
     mainPanel(
       tabsetPanel
       (
         tabPanel("Calculate the calories consumption",
                  p(h4("Calculation Results")),
                  textOutput("cc_result")
+        ),
+        tabPanel(
+          "Documentation",
+          p(h4("This app estimates the calories consumption")),
+          br(),
+          helpText("Fill in the hours of corresponding sports and click to calculate ")
         )
 
       )
